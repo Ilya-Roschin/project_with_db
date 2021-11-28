9f2f4fb27e5b1c8a1bdf279722d6e22c7ad4c11d
Ilya-Roschin <roshhin-78@mail.ru>
2021-11-28 13:34:25 +0300

    refactor project

--- src/main/java/com/java/training/application/dao/UserRepository.java
+++ src/main/java/com/java/training/application/dao/UserRepository.java
@@ -27,19 +27,20 @@ public class UserRepository implements Repository {
     public List<Entity> findAll(Class<? extends Entity> clazz) throws SQLException {
         Connection connection = CONNECTION_POOL.getConnection();
         LOGGER.info(MESSAGE_EXECUTING_STATEMENT);
-        Statement statement = connection.createStatement();
+        List<Entity> entities;
+        try (Statement statement = connection.createStatement()) {
 
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM user");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM user");
 
-        LOGGER.info("Retrieving data from database...");
-        System.out.println("\nuser:");
+            LOGGER.info("Retrieving data from database...");
+            System.out.println("\nuser:");
 
-        List<Entity> entities = new ArrayList<>();
-        entities.add(USER_MAPPER.mapTableToUser(resultSet));
+            entities = new ArrayList<>();
+            entities.add(USER_MAPPER.mapTableToUser(resultSet));
 
-        resultSet.close();
-        LOGGER.info(MESSAGE_CLOSE_STATEMENT);
-        statement.close();
+            resultSet.close();
+            LOGGER.info(MESSAGE_CLOSE_STATEMENT);
+        }
         LOGGER.info(MESSAGE_CLOSE_CONNECTION);
         CONNECTION_POOL.returnConnection(connection);
         return entities;
@@ -49,13 +50,13 @@ public class UserRepository implements Repository {
     public void deleteById(long id, String table, String column) throws SQLException {
         Connection connection = CONNECTION_POOL.getConnection();
         final String sql = "DELETE FROM " + table + " WHERE " + column + " = " + id;
-        PreparedStatement statement = connection.prepareStatement(sql);
-        LOGGER.info(MESSAGE_EXECUTING_STATEMENT);
-        LOGGER.info("Delete user from database...");
-        statement.executeUpdate(sql);
-        LOGGER.info("User " + "'" + id + "' deleted");
-        LOGGER.info(MESSAGE_CLOSE_STATEMENT);
-        statement.close();
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            LOGGER.info(MESSAGE_EXECUTING_STATEMENT);
+            LOGGER.info("Delete user from database...");
+            statement.executeUpdate(sql);
+            LOGGER.info("User " + "'" + id + "' deleted");
+            LOGGER.info(MESSAGE_CLOSE_STATEMENT);
+        }
         LOGGER.info(MESSAGE_CLOSE_CONNECTION);
         CONNECTION_POOL.returnConnection(connection);
     }
@@ -64,11 +65,11 @@ public class UserRepository implements Repository {
     public void insert(String data) throws SQLException {
         Connection connection = CONNECTION_POOL.getConnection();
         String sql = "INSERT INTO user (id_user, user_role, first_name, last_name, email, phone_number) VALUES " + data;
-        PreparedStatement statement = connection.prepareStatement(sql);
-        LOGGER.info(MESSAGE_EXECUTING_STATEMENT);
-        statement.executeUpdate(sql);
-        LOGGER.info(MESSAGE_CLOSE_STATEMENT);
-        statement.close();
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            LOGGER.info(MESSAGE_EXECUTING_STATEMENT);
+            statement.executeUpdate(sql);
+            LOGGER.info(MESSAGE_CLOSE_STATEMENT);
+        }
         LOGGER.info(MESSAGE_CLOSE_CONNECTION);
         CONNECTION_POOL.returnConnection(connection);
     }
